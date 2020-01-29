784a59c885db7b79ec8990ef90b802da493b4b96
Steve Dillon <steved@yaponcha.com>
2020-01-29 16:31:31 -0500

    flesh out the Readme some more

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 # vault-ca-demo
+
 A demonstration of how to setup vault to be a CA authority, using Terraform.  There is a [blog post](https://medium.com/@stvdilln/creating-a-certificate-authority-with-hashicorp-vault-and-terraform-4d9ddad31118) about how to use this repo.
 
-The articile I wrote and this repo need to be used together as there isn't an overview of how to use all the parts of this repo here. 
+The article I wrote and this repo need to be used together as there isn't an overview of how to use all the parts of this repo here.
+This repo demonstrates:
+
+* How to run vault locally with TLS options
+* How to Create a Self Signed cert in Terraform using the resource tls_self_signed_cert
+* How to use various vault_pki* resources:
+  * vault_pki_secret_backend_config_urls
+  * tls_self_signed_cert
+  * tls_private_key
+  * vault_pki_secret_backend_config_ca
+  * vault_pki_secret_backend_intermediate_cert_request
+  * vault_pki_secret_backend_root_sign_intermediate
+  * vault_pki_secret_backend_intermediate_set_signed
+* How to use a Vault Certificate Authority to create Client and Server TLS certs.
+* How to bootstrap a Vault instance to make it its own Certificate Authority
+
+
--- ca.tf
+++ ca.tf
@@ -1,3 +1,4 @@
+# Create a mount point for the Root Certiifacte authority.
 resource "vault_mount" "root" {
     type = "pki"
     path = "pki-root-ca"
@@ -6,21 +7,18 @@ resource "vault_mount" "root" {
     description = "Root Certificate Authority"
 }
 
+# Modify the mount point and set URLs for the issuer and crl.
 resource "vault_pki_secret_backend_config_urls" "config_urls" {
   depends_on = [ vault_mount.root ]  
   backend              = vault_mount.root.path
   issuing_certificates = ["http://vault1.${var.server_cert_domain}:8200/v1/pki/ca"]
   crl_distribution_points= ["http://vault1.${var.server_cert_domain}:8200/v1/pki/crl"]
 }
-# Take the Root CA certificate that we have created and store it in 
-# the mount point pki-root-ca.  The ca_pem_bundle in this case is
-# the Certificate we generated and the key for it.
-resource "vault_pki_secret_backend_config_ca" "ca_config" {
-  depends_on = [ vault_mount.root, tls_private_key.ca_key]  
-  backend  = vault_mount.root.path
-  pem_bundle = local_file.ca_pem_bundle.sensitive_content
-}
 
+# if you want to create the root cert in VAULT and never expose the 
+# private key to the local machine use this route.  However your 
+# CA infrastructure is now tied to vault and pretty much the server 
+# you created the CA on.
 # resource "vault_pki_secret_backend_root_cert" "ca-cert" {
 #   depends_on = ["vault_pki_secret_backend_config_urls.config_urls"]    
 #   backend = vault_mount.root.path
@@ -43,6 +41,22 @@ resource "vault_pki_secret_backend_config_ca" "ca_config" {
 #     file_permission = "0400"
 # }
 
+
+# Create a private key for use with the Root CA.
+resource tls_private_key ca_key {
+   algorithm = "RSA"
+   rsa_bits = 4096
+}
+# This is a highly sensitive output of this process.
+resource local_file private_key {
+    sensitive_content = tls_private_key.ca_key.private_key_pem
+    filename = "${path.root}/output/root_ca/ca_key.pem"
+    file_permission = "0400"
+}
+
+# 
+# Create a Self Signed Root Certificate Authority
+#
 resource tls_self_signed_cert ca_cert {
    private_key_pem = tls_private_key.ca_key.private_key_pem
    key_algorithm = "RSA"
@@ -66,24 +80,26 @@ resource tls_self_signed_cert ca_cert {
    is_ca_certificate = true 
 
 }
-resource tls_private_key ca_key {
-   algorithm = "RSA"
-   rsa_bits = 4096
-}
 
-resource local_file private_key {
-    sensitive_content = tls_private_key.ca_key.private_key_pem
-    filename = "${path.root}/output/root_ca/ca_key.pem"
-    file_permission = "0400"
-}
 resource local_file ca_file {
     sensitive_content = tls_self_signed_cert.ca_cert.cert_pem
     filename = "${path.root}/output/root_ca/ca_cert.pem"
     file_permission = "0400"
 }
+# This PEM bundle is not like most others.  Most PEM bundles are a chain of Certificate Authority Certs.
+# This bundle is the Private Key (first), followed by the the Certificate.
 resource local_file ca_pem_bundle {
     sensitive_content = "${tls_private_key.ca_key.private_key_pem}${tls_self_signed_cert.ca_cert.cert_pem}"
     filename = "${path.root}/output/root_ca/ca_cert_key_bundle.pem"
     file_permission = "0400"
+}
+
+# Take the Root CA certificate that we have created and store it in 
+# the mount point pki-root-ca.  The ca_pem_bundle in this case is
+# the Certificate we generated and the key for it.
+resource "vault_pki_secret_backend_config_ca" "ca_config" {
+  depends_on = [ vault_mount.root, tls_private_key.ca_key]  
+  backend  = vault_mount.root.path
+  pem_bundle = local_file.ca_pem_bundle.sensitive_content
 }
 
